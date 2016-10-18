112add7c177f920e4840dc239623651fc05e6f69
木头 <mutou@lanlanlife.com>
2016-10-19 00:20:57 +0800

    添加index

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import include,url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    # 默认目录
     url(r'^', include('mainSite.urls')),
+    # 管理目录
+    url(r'^admin/', admin.site.urls),
 ]
