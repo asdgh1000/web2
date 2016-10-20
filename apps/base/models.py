5be02e6f9aee84b43d39c7fb478f9a94da32a2e2
木头 <mutou@lanlanlife.com>
2016-10-21 01:14:31 +0800

    bug fix

--- apps/base/models.py
+++ apps/base/models.py
@@ -5,3 +5,6 @@ class BaseModel(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     deleted = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
