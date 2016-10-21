66d3d221c075a0ad958d29125739fc4e8243a1d2
木头 <mutou@lanlanlife.com>
2016-10-21 16:28:22 +0800

    保存blog信息

---
+++ apps/myblog/settings.py
@@ -0,0 +1,7 @@
+import os
+
+#博客文件目录
+BLOG_FILE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static/blog_file/')
+
+#博客封面路径
+BLOG_COVER_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static/blog_cover/')
