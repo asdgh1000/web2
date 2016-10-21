736ea64cbf986c47a2484f1a2ac65c93e37b3dce
木头 <mutou@lanlanlife.com>
2016-10-21 23:26:12 +0800

    页面优化

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,7 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL ='/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, "apps/myblog/static"),
