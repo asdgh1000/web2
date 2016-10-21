016810a615d3cc8c81486dd8471e7efbf49065c6
木头 <mutou@lanlanlife.com>
2016-10-21 15:02:36 +0800

    添加表单验证提示

--- apps/myblog/forms.py
+++ apps/myblog/forms.py
@@ -3,7 +3,7 @@ from django import forms
 
 class UploadBlogForm(forms.Form):
 
-    file = forms.FileField()
-    file_name = forms.CharField(max_length=5)
-    title = forms.CharField(max_length=255)
-    cover_img = forms.FileField(required=False)
+    file = forms.FileField(label="博客文件")
+    file_name = forms.CharField(max_length=5, label="文件名")
+    title = forms.CharField(max_length=255, label="标题")
+    cover_img = forms.FileField(required=False, label="封面图")
