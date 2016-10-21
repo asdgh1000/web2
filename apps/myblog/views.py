cc6fd2fc2329cde88d9c4ae31be5100028d67f39
木头 <mutou@lanlanlife.com>
2016-10-21 12:21:11 +0800

    bug fix

--- apps/myblog/views.py
+++ apps/myblog/views.py
@@ -11,17 +11,14 @@ def index(request):
 
 
 def upload_blog(request):
-    base_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static/blog_file')
+    base_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static/blog_file/')
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             blog_fd = request.FILES['file']
             file_name = form.data.get('file_name')
             save_file(blog_fd, base_path + file_name)
-            return HttpResponseRedirect('myblog/index.html')
-        else:
-            raise Http404("表单验证失败")
-
+            return HttpResponseRedirect('/')
     else:
         form = UploadFileForm()
-    return render_to_response('myblog/upload_blog.html', {'form': form})
+    return render(request, 'myblog/upload_blog.html', {'form': form})
