223ca403ac0a5b7bb7c4167c87bed29804fc034d
木头 <mutou@lanlanlife.com>
2016-10-21 15:48:10 +0800

    添加表单验证提示

--- apps/base/utils.py
+++ apps/base/utils.py
@@ -7,6 +7,7 @@ def save_file(fd, file_absolute_path):
     :param file_absolute_path: 文件绝对路径
     :return:
     '''
+
     with open(file_absolute_path, 'wb+') as destination:
         for chunk in fd.chunks():
             destination.write(chunk)
