47eea9552897c9b80273cd6ffcbd266fb28a72a0
木头 <mutou@lanlanlife.com>
2016-10-21 11:53:34 +0800

    bug fix

--- apps/myblog/views.py
+++ apps/myblog/views.py
@@ -13,11 +13,11 @@ def upload_blog(request):
     base_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static/blog_file')
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
-        blog_fd = request.FILES['file']
-        file_name = form.file_name
         if form.is_valid():
+            blog_fd = request.FILES['file']
+            file_name = form.file_name
             save_file(blog_fd, base_path + file_name)
             return render(request, 'myblog/index.html')
     else:
         form = UploadFileForm()
-        return render_to_response('myblog/upload_blog.html', {'form': form})
+    return render_to_response('myblog/upload_blog.html', {'form': form})
