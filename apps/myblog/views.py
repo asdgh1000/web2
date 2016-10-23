576f46d0db75004f06b1b5063666709240c91227
木头 <mutou@lanlanlife.com>
2016-10-23 23:24:55 +0800

    页面优化

--- apps/myblog/views.py
+++ apps/myblog/views.py
@@ -66,7 +66,7 @@ def blog_detail(request, blog_info_id):
 
 
 def blog_list(request, page):
-    blog_info_list = BlogInfo.objects.all()
+    blog_info_list = BlogInfo.objects.order_by("-created")
     paginator = Paginator(blog_info_list, 25)  # Show 25 contacts per page
 
     try:
