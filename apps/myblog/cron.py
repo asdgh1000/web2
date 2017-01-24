7f0dd09fe340416d8b94f95084b8d3e7ff6a6acf
木头 <mutou@lanlanlife.com>
2017-01-24 15:46:27 +0800

    bug fix

--- apps/myblog/cron.py
+++ apps/myblog/cron.py
@@ -7,7 +7,7 @@ def sync_blog_click_count():
     redis_client = get_redis_connection()
     keys = redis_client.scan_iter("%s%s" % (BLOG_CLICK_PREFIX, '*'))
     for key in keys:
-        blog_id = key[19:]
+        blog_id = key[len(BLOG_CLICK_PREFIX)-1:]
         count = redis_client.getset("%s%s" % (BLOG_CLICK_PREFIX, blog_id))
         blogInfo = BlogInfo.objects.filter(blog_id=blog_id)
         blogInfo.click_count += count
